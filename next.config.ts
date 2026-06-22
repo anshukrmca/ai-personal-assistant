43b6bbc1e3b5b8281bbd119e0d04d8fa913088c9
Anshu Kumar <anshukumarmca@gmail.com>
2026-06-22 13:29:15 +0530

    chore: add firebase-admin and jwks-rsa to serverExternalPackages in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Configuration options
+  serverExternalPackages: ["firebase-admin", "jwks-rsa"],
 };
 
 export default nextConfig;
